3650f95b571438c69ab03f5deaee43c524cedd5c
DanielePilati <danielepdj@gmail.com>
2024-05-10 03:50:21 +0200

    [upd] add second bar

--- main.js
+++ main.js
@@ -15,39 +15,49 @@ form.addEventListener("submit", function (event) {
   console.log(rate);
   console.log(price);
 
+  document.getElementById("refreshmessage").classList.toggle("d-none");
+  document.getElementById("myBtn").classList.toggle("d-none");
+  document.getElementById("reset").classList.toggle("d-none");
+  document.getElementById("banner").classList.toggle("d-none");
+  document.getElementById("banner-two").classList.toggle("d-none");
+
   if (age.value > 0 && km.value > 0 && age.value < 130) {
-    document.getElementById("banner").classList.toggle("d-none");
     if (age.value < 18) {
       price = price / 1.2;
       console.log("sei minorenne 20% di sconto");
-      document.getElementById("message").innerHTML = `Hai ${
-        age.value
-      } anni ed hai diritto al 20% di sconto. 
-        Il prezzo del tuo biglietto è di ${price.toFixed(2)} € anzichè ${(
-        price * 1.2
-      ).toFixed(2)} €`;
+      document.getElementById(
+        "message"
+      ).innerHTML = `Hai ${age.value} anni ed hai diritto al 20% di sconto.`;
+      document.getElementById(
+        "message-two"
+      ).innerHTML = `Il prezzo del tuo biglietto è di ${price.toFixed(
+        2
+      )} € anzichè ${(price * 1.2).toFixed(2)} €`;
     } else if (age.value > 65) {
       price = price / 1.4;
       console.log("sei over 65 40% di sconto");
-      document.getElementById("message").innerHTML = `Hai ${
-        age.value
-      } anni ed hai diritto al 40% di sconto. 
+      document.getElementById(
+        "message"
+      ).innerHTML = `Hai ${age.value} anni ed hai diritto al 40% di sconto.`;
+      document.getElementById("message-two").innerHTML = `
         Il prezzo del tuo biglietto è di ${price.toFixed(2)} € anzichè ${(
         price * 1.4
       ).toFixed(2)} €`;
     } else {
       console.log("no sconto");
-      document.getElementById("message").innerHTML = `Hai ${
-        age.value
-      } anni e non hai diritto a sconto. 
-        Il prezzo del tuo biglietto è di ${price.toFixed(2)} € (prezzo pieno)`;
+      document.getElementById(
+        "message"
+      ).innerHTML = `Hai ${age.value} anni e non hai diritto a sconto.`;
+      document.getElementById(
+        "message-two"
+      ).innerHTML = `Il prezzo del tuo biglietto è di ${price.toFixed(
+        2
+      )} € (prezzo pieno)`;
     }
   } else {
     console.log("inserisci valori validi");
-    document.getElementById("banner").classList.toggle("d-none");
     document.getElementById(
       "message"
-    ).innerHTML = `Hai inserito ${age.value} anni e ${km.value} Km.
-          Questi valori non sono validi, premi "Reset" e riprova`;
+    ).innerHTML = `Questi valori non sono validi`;
   }
 });
